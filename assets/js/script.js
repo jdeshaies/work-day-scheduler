fe493b36ffeeb64a628827112fc083789c3175bc
Joey <josephdeshaies@Joeys-MacBook-Pro-2.local>
2023-02-04 15:46:00 -0500

    Adds current day to header and updates link to js and css files

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,5 +19,8 @@ $(function () {
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
-  // TODO: Add code to display the current date in the header of the page.
+  // Displays the current date in the header of the page using dayjs()
+  var currentDateEl = $('#currentDay');
+  var today = dayjs();
+  currentDateEl.text(today.format('dddd, MMMM D[th]'));
 });
