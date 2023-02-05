a57ebcd8c6d45ef1be47c60cf86de87700d4b593
Joey <josephdeshaies@Joeys-MacBook-Pro-2.local>
2023-02-04 19:49:01 -0500

    Adds currentHour variable and updates time block IDs using 24 hour time

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,11 +17,8 @@ $(function () {
   // useful when saving the description in local storage?
   $('.saveBtn').on('click', function() {
     var hourSaved = $(this).parent().attr('id');
-    console.log('Hour' + hourSaved);
     var textSaved = $(this).prev().val().trim();
-    console.log('Text' + textSaved);
     userInput[hourSaved] = textSaved;
-    console.log(userInput);
     localStorage.setItem('userInput', JSON.stringify(userInput));
   });
 
@@ -31,6 +28,8 @@ $(function () {
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
+  var currentHour = dayjs().format('h');
+  console.log('Current Hour: ' +currentHour);
   
 
   // TODO: Add code to get any user input that was saved in localStorage and set
